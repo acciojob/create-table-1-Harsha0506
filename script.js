d896a66c179168d1e3ecb1b21948020897646075
Harsha0506 <110712960+Harsha0506@users.noreply.github.com>
2026-02-19 13:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let table = document.getElementById("sampleTable");
+let rowNumber = 3;  // start from 3
+
 function insert_Row() {
     //Write your code here
+	let row = document.createElement("tr");
+
+    let cell1 = document.createElement("td");
+    cell1.textContent = `Row ${rowNumber} Cell 1`;
+
+    let cell2 = document.createElement("td");
+    cell2.textContent = `Row ${rowNumber} Cell 2`;
+
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+
+    table.appendChild(row);
+
+    rowNumber++;  // increase for next click
   
   
 }
